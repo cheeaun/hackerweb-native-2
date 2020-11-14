e7e8adff521c66e6ec8779a2518acd398a45e2e6
Lim Chee Aun <cheeaun@gmail.com>
2020-11-14 18:15:08 +0800

    Remove unused ModalStatusBar

--- components/ModalStatusBar.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-// Fix wrong statusbar color when modal is up on iOS, for Light Mode
-import { StatusBar } from 'expo-status-bar';
-export default function () {
-  return <StatusBar style="inverted" animated />;
-}
