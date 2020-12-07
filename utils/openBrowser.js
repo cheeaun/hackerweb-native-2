418f8388c8351ce3cc3e8a885398fac5183b9f04
Lim Chee Aun <cheeaun@gmail.com>
2020-12-07 09:52:10 +0800

    WebBrowser needs encodeURI, else it’ll crash
    Add Alert to notify user, but it’s a pretty unrecoverable crash.

--- utils/openBrowser.js
+++ utils/openBrowser.js
@@ -1,7 +1,10 @@
 import * as WebBrowser from 'expo-web-browser';
+import { Alert } from 'react-native';
 
 export default function (url) {
-  return WebBrowser.openBrowserAsync(url, {
+  return WebBrowser.openBrowserAsync(encodeURI(url), {
     enableBarCollapsing: true,
+  }).catch((e) => {
+    Alert.alert('An error occured', e.message || e.toString());
   });
 }
