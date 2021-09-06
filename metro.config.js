c63ff0be6f638ed7904f3a0c6423f767b6cd2ceb
Lim Chee Aun <cheeaun@gmail.com>
2021-09-06 11:42:28 +0800

    Maybe this will fix it

--- metro.config.js
+++ metro.config.js
@@ -1,18 +1,12 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const config = getDefaultConfig(__dirname);
 
-const defaultConfig = getDefaultConfig(__dirname);
+config.transformer.babelTransformerPath = require.resolve(
+  'react-native-svg-transformer',
+);
+config.resolver.assetExts = config.resolver.assetExts.filter(
+  (ext) => ext !== 'svg',
+);
+config.resolver.sourceExts.push('svg');
 
-module.exports = (async () => {
-  const {
-    resolver: { sourceExts, assetExts },
-  } = defaultConfig;
-  return {
-    transformer: {
-      babelTransformerPath: require.resolve('react-native-svg-transformer'),
-    },
-    resolver: {
-      assetExts: assetExts.filter((ext) => ext !== 'svg'),
-      sourceExts: [...sourceExts, 'svg'],
-    },
-  };
-})();
+module.exports = config;
