b588140619a2d1633828b25845a7478f96a39e5c
Lim Chee Aun <cheeaun@gmail.com>
2022-04-20 22:33:37 +0800

    Use new HTML parser: parse5
    Slower but more spec-compliant and reliable

---
+++ components/HTMLView2.jsx
@@ -0,0 +1,181 @@
+import React, { useState, useEffect } from 'react';
+import {
+  StyleSheet,
+  View,
+  ScrollView,
+  PlatformColor,
+  DynamicColorIOS,
+  useWindowDimensions,
+} from 'react-native';
+import { parseFragment } from 'parse5';
+import urlRegexSafe from 'url-regex-safe';
+import * as entities from 'entities';
+import stripIndent from 'strip-indent';
+
+import Text from './Text';
+
+import openBrowser from '../utils/openBrowser';
+import openShare from '../utils/openShare';
+
+const baseFontSize = 15;
+const nodeStyles = StyleSheet.create({
+  default: {
+    fontSize: baseFontSize,
+  },
+  p: {
+    marginBottom: 12,
+    fontSize: baseFontSize,
+  },
+  blockquote: {
+    marginBottom: 12,
+    fontSize: baseFontSize,
+    backgroundColor: DynamicColorIOS({
+      dark: 'rgba(255,255,255,.05)',
+      light: 'rgba(0,0,0,.05)',
+    }),
+    padding: 8,
+    opacity: 0.8,
+  },
+  pre: {
+    backgroundColor: DynamicColorIOS({
+      dark: 'rgba(255,255,255,.05)',
+      light: 'rgba(0,0,0,.05)',
+    }),
+    borderRadius: 4,
+    marginBottom: 12,
+  },
+  preInner: {
+    padding: 10,
+  },
+  code: {
+    fontFamily: 'Menlo',
+    fontSize: baseFontSize - 2,
+  },
+  a: {
+    color: PlatformColor('link'),
+    fontSize: baseFontSize,
+  },
+  i: {
+    fontStyle: 'italic',
+    fontSize: baseFontSize,
+  },
+});
+
+const onLinkPress = (url) => {
+  openBrowser(url);
+};
+const onLinkLongPress = (url) => {
+  openShare({ url });
+};
+
+function PreView({ children, ...props }) {
+  const windowHeight = useWindowDimensions().height;
+  return (
+    <ScrollView
+      automaticallyAdjustContentInsets={false}
+      scrollsToTop={false}
+      style={[nodeStyles.pre, { maxHeight: windowHeight * 0.5 }]}
+      decelerationRate={0} // Easier to read the code
+      {...props}
+    >
+      <View style={nodeStyles.preInner} onStartShouldSetResponder={() => true}>
+        {children}
+      </View>
+    </ScrollView>
+  );
+}
+
+function dom2elements(nodes, parentName) {
+  if (!nodes || !nodes.length) return;
+  return nodes.map((node) => {
+    const { tagName, nodeName, childNodes } = node;
+    const key = nodeName + '-' + Math.random();
+    if (tagName) {
+      const style = nodeStyles[tagName || 'default'];
+      var elements = dom2elements(childNodes, tagName);
+      if (!elements) return null;
+      if (tagName === 'pre') {
+        return <PreView key={key}>{elements}</PreView>;
+      }
+      if (tagName === 'a') {
+        const href = node.attrs?.find((attr) => attr.name === 'href').value;
+        // Steps to make sure children inside is ACTUALLY text
+        const child = childNodes?.length === 1;
+        const text = child?.nodeName === '#text' && child.value;
+        return (
+          <Text
+            key={key}
+            style={style}
+            onPress={onLinkPress.bind(null, href)}
+            onLongPress={onLinkLongPress.bind(null, href)}
+          >
+            {text || elements}
+          </Text>
+        );
+      }
+      if (tagName === 'p') {
+        let firstChild = childNodes && childNodes[0];
+        if (firstChild.tagName) {
+          // Sometimes can be an <i> tag
+          firstChild = firstChild.childNodes && firstChild.childNodes[0];
+        }
+        const firstText =
+          firstChild && firstChild.nodeName === '#text' && firstChild.value;
+        if (
+          (firstText && /^>{1,2}[^<>]+$/.test(firstText)) ||
+          firstText === '>'
+        ) {
+          return (
+            <Text key={key} style={nodeStyles.blockquote}>
+              {elements}
+            </Text>
+          );
+        }
+      }
+      return (
+        <Text key={key} style={style}>
+          {elements}
+        </Text>
+      );
+    } else if (nodeName === '#text') {
+      const style = nodeStyles[parentName || 'default'];
+      const { value } = node;
+      let text;
+      if (parentName === 'code') {
+        // Trim EOL newline
+        text = stripIndent(value.replace(/\n$/, ''));
+      } else {
+        // Trim ALL newlines, because HTML
+        text = value.replace(/[\n\s\t]+/g, ' ');
+      }
+      return (
+        <Text key={key} style={style}>
+          {text}
+        </Text>
+      );
+    }
+  });
+}
+
+const urlRegex = urlRegexSafe({
+  localhost: false,
+  strict: true,
+});
+
+export default function HTMLView2({ html, linkify }) {
+  if (!html || !html.trim()) return null;
+  if (linkify) {
+    const containsLink = /<\/a>/i.test(html);
+    if (containsLink) {
+      console.warn('HTML contains anchors and linkify=true', html);
+    } else {
+      html = entities
+        .decodeHTML(html)
+        .replace(/(<\w)/gi, '\n$1') // Some tags are too "sticky"
+        .replace(urlRegex, (url) => `<a href="${url}">${url}</a>`);
+    }
+  }
+  const docFrag = parseFragment(html);
+  const elements = dom2elements(docFrag.childNodes);
+  return elements;
+}
--- screens/DevTestScreen.jsx
+++ screens/DevTestScreen.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { View } from 'react-native';
 import Text from '../components/Text';
 import HTMLView from '../components/HTMLView';
+import HTMLView2 from '../components/HTMLView2';
 
 export default function DevTestScreen() {
   return (
@@ -11,6 +12,13 @@ export default function DevTestScreen() {
         html={`<p>&test <a href="https://google.com/">google.com</a> test</p><p>&gt; test <a href="https://google.com/">google.com</a> test</p><p>&gt; test <a href="https://google.com/">google.com</a> test</p>`}
       />
       <HTMLView
+        html={`<p><i>&gt; <a href="https://google.com/">google.com</a> test<p>&gt; test <a href="https://google.com/">google.com</a> test<p>test <a href="https://google.com/">google.com</a> test</p>`}
+      />
+      <Text bolder>HTMLView2</Text>
+      <HTMLView2
+        html={`<p>&test <a href="https://google.com/">google.com</a> test</p><p>&gt; test <a href="https://google.com/">google.com</a> test</p><p>&gt; test <a href="https://google.com/">google.com</a> test</p>`}
+      />
+      <HTMLView2
         html={`<p><i>&gt; <a href="https://google.com/">google.com</a> test<p>&gt; test <a href="https://google.com/">google.com</a> test<p>test <a href="https://google.com/">google.com</a> test</p>`}
       />
     </View>
