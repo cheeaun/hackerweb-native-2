453c17d5616275f31b860ece001c08d3d25747c2
Lim Chee Aun <cheeaun@gmail.com>
2022-04-29 09:30:42 +0800

    Seems like no need encode now?

--- utils/openBrowser.js
+++ utils/openBrowser.js
@@ -3,7 +3,7 @@ import { Alert } from 'react-native';
 import * as WebBrowser from 'expo-web-browser';
 
 export default function (url) {
-  return WebBrowser.openBrowserAsync(encodeURI(url), {
+  return WebBrowser.openBrowserAsync(url, {
     enableBarCollapsing: true,
   }).catch((e) => {
     Alert.alert('An error occured', e.message || e.toString());
