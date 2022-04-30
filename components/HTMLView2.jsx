309cf076dafa577ecea0a1f1a68cde638acb6d99
Lim Chee Aun <cheeaun@gmail.com>
2022-04-30 21:05:09 +0800

    I seriously need a better debugging tool

--- components/HTMLView2.jsx
+++ components/HTMLView2.jsx
@@ -478,8 +478,8 @@ export default function HTMLView2({ html, linkify, DEBUG }) {
 
   const docFrag = parseFragment(html);
   const elements = dom2elements(docFrag.childNodes);
-  if (__DEV__ && DEBUG) {
-    console.log({ html });
+  if (__DEV__) {
+    if (DEBUG) console.log({ html });
     return (
       <Pressable
         onLongPress={() => {
