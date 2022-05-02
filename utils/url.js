b4aa5c46bf919435510bbf22ea4892db31ffac00
Lim Chee Aun <cheeaun@gmail.com>
2022-05-02 17:19:18 +0800

    Add comment to remind self in future

--- utils/url.js
+++ utils/url.js
@@ -1,5 +1,8 @@
 const isHTTPLink = (url) => /^https?:/.test(url);
 
+// NOTE: Might not work
+// Responses from Algolia and Firebase don't return url
+// if item is a story without link
 const isHNPage = (url) => /^item/i.test(url);
 
 export { isHTTPLink, isHNPage };
