f642cc95ee19083807a36a9ba06de03b846672c3
Lim Chee Aun <cheeaun@gmail.com>
2022-05-06 19:34:13 +0800

    It's time for new presentation style for WebBrowser

--- utils/openBrowser.js
+++ utils/openBrowser.js
@@ -5,6 +5,7 @@ import * as WebBrowser from 'expo-web-browser';
 export default function (url) {
   return WebBrowser.openBrowserAsync(url, {
     enableBarCollapsing: true,
+    presentationStyle: 'automatic',
   }).catch((e) => {
     Alert.alert('An error occured', e.message || e.toString());
   });
